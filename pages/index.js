465f1011f1348d28efabc1544928d21fcefe865a
Allan Nascimento <nascimento.allang@gmail.com>
2025-11-28 16:38:22 -0300

    Initial Manifest

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,39 @@
 function Home() {
-  return <h1>Em construção</h1>;
+  return <h1>Allan Nascimento</h1>;
+  <p>
+    Para muitos, a tecnologia é apenas uma ferramenta fria. Para mim, ela é a
+    mais pura busca pela ordem.
+    <br></br>
+    <br></br>
+    Essa busca não se contenta apenas com o o quê deve ser feito, mas vive da
+    obsessão pelo como e o porquê. Vejo uma arte mística em encarar um problema
+    caótico e esculpi-lo pacientemente até que a confusão dê lugar a um sistema
+    que respira, funciona e existe por conta própria.
+    <br></br>
+    <br></br>É por isso que trabalho: para sentir o êxtase de ver a engrenagem
+    girar sozinha. É a sensação de ser hipnotizado pela própria criação, vendo a
+    ordem emergir do caos de forma tão natural que parece inevitável.
+    <br></br>
+    <br></br>
+    Criar algo assim exige a coragem de mergulhar fundo. Enquanto o mercado
+    corre atrás de atalhos superficiais, escolhi o caminho difícil da
+    profundidade. Defendo que a verdadeira segurança só existe quando dominamos
+    os fundamentos e entendemos a alma da máquina, não apenas seus botões.
+    <br></br>
+    <br></br>
+    Não sou o detentor de todo o conhecimento do mundo, e é exatamente isso que
+    me move. Sou impulsionado por uma curiosidade insaciável e pela habilidade
+    vital de navegar pelo desconhecido para encontrar a resposta que ninguém
+    mais encontrou. O meu trabalho é exterminar a confusão diante do novo,
+    criando sistemas tão bem feitos que você esqueça que eles são complexos.
+    <br></br>
+    <br></br>
+    Busco incansavelmente a excelência e sinto repulsa pelo medíocre, mas isso
+    não me torna imune a falhas. A minha promessa é a transparência radical:
+    encarar cada erro não como um fracasso, mas como o combustível necessário
+    para forjar soluções definitivas. Se você valoriza a verdade acima da
+    facilidade, convido você a trilhar comigo esse caminho em direção à ordem.
+  </p>;
 }
 
 export default Home;
